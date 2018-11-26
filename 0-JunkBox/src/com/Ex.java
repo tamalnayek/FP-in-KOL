39718cf0e08a74c383969f375c8e91d80d9ebce3
Nag <nagabhushanamn@gmail.com>
2018-11-26 15:57:44 +0530

    FP

---
+++ 0-JunkBox/src/com/Ex.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.util.function.Supplier;
+
+public class Ex {
+
+	public static void main(String[] args) {
+
+		Supplier<String> supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+		supplier = () -> {
+			return null;
+		};
+	}
+
+}
